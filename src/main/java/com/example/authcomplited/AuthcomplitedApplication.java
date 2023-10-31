8bf6f84269b58cc19769bf4417c2b6d21904ea70
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-10-31 01:30:47 +0100

    Initial commit

---
+++ src/main/java/com/example/authcomplited/AuthcomplitedApplication.java
@@ -0,0 +1,13 @@
+package com.example.authcomplited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcomplitedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcomplitedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authcomplited/AuthcomplitedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authcomplited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcomplitedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
